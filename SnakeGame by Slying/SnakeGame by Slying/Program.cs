2635bff23084c0357e1f6adb219439f1f1f1b368
Slying <147253589+slyingg@users.noreply.github.com>
2023-10-31 10:43:23 +0100

    Add files via upload

---
+++ SnakeGame by Slying/SnakeGame by Slying/Program.cs
@@ -0,0 +1,23 @@
+﻿using Classic_Snakes_Game_by_Slying;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame_by_Slying
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ SnakeGame by Slying/SnakeGame by Slying/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Classic_Snakes_Game_by_Slying
+
+{
+    class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static string directions;
+        public Settings()
+        {
+            Width = 17;
+            Height = 17;
+            directions = "left";
+        }
+    }
+}
